9bf62aec70ebd43d89b6d35334f50fc7e44d83e3
PZ-Scripts <maxwollesen9@gmail.com>
2025-09-03 15:00:52 +0200

    Update of client
    From dk to en

--- client.lua
+++ client.lua
@@ -2,28 +2,28 @@ local ox_target = exports.ox_target
 
 ox_target:addGlobalPlayer({
     {
-        label = 'Sæt i strips',
+        label = 'Put in zipties',
         icon = 'fa-solid fa-user-lock',
         onSelect = function(data)
             TriggerServerEvent('belmont:putInStrips', data.entity)
         end
     },
     {
-        label = 'Tag strips af',
+        label = 'Remove zipties',
         icon = 'fa-solid fa-unlock',
         onSelect = function(data)
             TriggerServerEvent('belmont:removeStrips', data.entity)
         end
     },
     {
-        label = 'Sæt i bil',
+        label = 'Put in car',
         icon = 'fa-solid fa-car-side',
         onSelect = function(data)
             TriggerServerEvent('belmont:putInVehicle', data.entity)
         end
     },
     {
-        label = 'Tag ud af bil',
+        label = 'Remove from car',
         icon = 'fa-solid fa-door-open',
         onSelect = function(data)
             TriggerServerEvent('belmont:removeFromVehicle', data.entity)
